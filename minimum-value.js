0763332191bdee3b33ba446b9573e862a98c5ef5
Aisha1609 <aishahzeidan98@gmail.com>
2025-07-30 20:47:19 +0300

    Added whiteboard image

---
+++ minimum-value.js
@@ -0,0 +1,15 @@
+# Challenge 02: Minimum Value
+
+## Problem Domain
+Write a function called `MinimumValue` that accepts an array of integers. Without using any built-in methods, the function should return the smallest value in the array.
+
+## Example
+| Input                 | Output |
+|----------------------|--------|
+| [10, 15, 5, 6, 12]    | 5      |
+| [11, 3, 5, 7, 9]      | 3      |
+| [17, 9, 13, 25, 5]    | 5      |
+
+## Whiteboard Solution
+
+![Minimum Value](./whiteboard-challenges/solution.jpg)
